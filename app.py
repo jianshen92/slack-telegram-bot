8b056ebc65d5550178c1b2cf91c2067df68ef9c4
Jian Shen Yap <jianshen92@gmail.com>
2020-03-15 22:20:29 +0800

    Fix

--- app.py
+++ app.py
@@ -24,35 +24,35 @@ def hello():
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_TOKEN'], "/slack/events", app)
 
 # Create an event listener for "reaction_added" events and print the emoji name
-@slack_events_adapter.on("reaction_added")
-def reaction_added(event_data):
-	emoji = event_data["event"]["reaction"]
-
-	slack_client.chat_postMessage(
-		channel="bots-talk",
-		text=f"{emoji}"
-	)
-
-@slack_events_adapter.on("message")
-def message_channel(event_data):
-	# print(json.dumps(event_data))
-	if "bot_id" in event_data["event"]:
-		return
-	#
-	# import pdb
-	# pdb.set_trace()
-	text = event_data["event"]["text"]
-	print(text)
-
-	slack_client.chat_postMessage(
-		channel="bots-talk",
-		text=f"{text}"
-	)
+# @slack_events_adapter.on("reaction_added")
+# def reaction_added(event_data):
+# 	emoji = event_data["event"]["reaction"]
+#
+# 	slack_client.chat_postMessage(
+# 		channel="bots-talk",
+# 		text=f"{emoji}"
+# 	)
+#
+# @slack_events_adapter.on("message")
+# def message_channel(event_data):
+# 	# print(json.dumps(event_data))
+# 	if "bot_id" in event_data["event"]:
+# 		return
+# 	#
+# 	# import pdb
+# 	# pdb.set_trace()
+# 	text = event_data["event"]["text"]
+# 	print(text)
+#
+# 	slack_client.chat_postMessage(
+# 		channel="bots-talk",
+# 		text=f"{text}"
+# 	)
 
 
 @slack_events_adapter.on("app_mention")
 def mention_me(event_data):
-	print("sup")
+	print("--Notification sent to telegram")
 	send_telegram_notif()
 
 # Start the server on port 3000
--- send_telegram.py
+++ send_telegram.py
@@ -1,10 +1,14 @@
 import urllib, requests
+import os
 
-tele_token = "1035031162:AAHbsYDVpHcxRg0NLJQpgi_VZGyF_gH1gz4"
-chat_id = "637286262"
+# tele_token = "1035031162:AAHbsYDVpHcxRg0NLJQpgi_VZGyF_gH1gz4"
+# chat_id = "637286262"
+
+tele_token = os.environ["TELEGRAM_TOKEN"]
+chat_id = os.environ["TELEGRAM_CHAT_ID"]
 
 def send_telegram_notif():
 	url = 'https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (
-		tele_token, chat_id, urllib.parse.quoteplus('A message has been posted in slack'))
+		tele_token, chat_id, urllib.parse.quote_plus('A message has been posted in slack'))
 
 	_ = requests.get(url, timeout=10)
