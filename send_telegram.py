b8a31eae455e36e27cb9d5b8d50e2867ba635f65
Ooi Sze Shen <ooiszeshen@gmail.com>
2020-03-16 20:36:27 +0800

    Edited message

--- send_telegram.py
+++ send_telegram.py
@@ -1,14 +1,11 @@
 import urllib, requests
 import os
 
-# tele_token = "1035031162:AAHbsYDVpHcxRg0NLJQpgi_VZGyF_gH1gz4"
-# chat_id = "637286262"
-
 tele_token = os.environ["TELEGRAM_TOKEN"]
 chat_id = os.environ["TELEGRAM_CHAT_ID"]
 
 def send_telegram_notif():
 	url = 'https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (
-		tele_token, chat_id, urllib.parse.quote_plus('A message has been posted in slack'))
+		tele_token, chat_id, urllib.parse.quote_plus('An important message has been posted in slack! Please check :)'))
 
 	_ = requests.get(url, timeout=10)
